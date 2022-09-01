e09b6378793023757dc61b9a4cc0f9dc49caf489
Ahmad Ali <ahmad.ali78643@gmail.com>
2022-09-01 22:36:08 +0500

    Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,7 +20,7 @@ class DashboardController extends Controller
       $this->newMembers = FeeStructure::where('admission_date', 'Like', '%' . $currentYearMonth. '%')->count();
       $this->pendingDues = $FeePendingMembers->count();
     }
-    $unPaidMembers = $FeePendingMembers->get()->random(5);
+    $unPaidMembers = $FeePendingMembers->get();
     return view('dashboard', compact('totalMembers', 'totalAmount', 'unPaidMembers'))
       ->with('newMembers', $this->newMembers)
       ->with('pendingDues', $this->pendingDues);
